3c238ac70ffce10836bab9c335991844e0ebaca9
Olga <148773307+OlgaGrief@users.noreply.github.com>
2024-03-12 08:20:39 +0300

    Delete src/test/java/Data directory

--- src/test/java/Data/Constants.java
+++
@@ -1,5 +0,0 @@
-package Data;
-
-public class Constants {
-    public final static float DELTA = 0.00001f;
-}
--- src/test/java/Data/TestDataRandom.java
+++
@@ -1,9 +0,0 @@
-package Data;
-
-import org.apache.commons.lang3.RandomStringUtils;
-
-public class TestDataRandom {
-    public final static float randomPrice = Float.valueOf(RandomStringUtils.randomNumeric(2));
-    public final static String randomName = RandomStringUtils.randomAlphabetic(12);
-}
-
